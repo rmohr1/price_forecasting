8025c565bfb706357f99bfbfa613bee94ae46db3
Rob Mohr <rcmohr4@gmail.com>
2025-07-02 09:46:28 -0400

    Refactored DAM demand data pull script

--- src/data/pull_load_data.py
+++ src/data/pull_load_data.py
@@ -1,60 +1,28 @@
-from datetime import datetime, timedelta
-import os
-import time
-import pandas as pd
-from pycaiso.oasis import SystemDemand
+from pull_data import fetch_and_cache_data
+
 
 # CONFIGURATION
-SAVE_DIR = "../../data/raw/caiso_demand_2DA"  # Directory to save cached files
+QUERYNAME = 'SLD_FCST' #Report to query
+MARKET = "DAM"  # Real-Time Market
+SAVE_DIR = "../../data/raw/dam_dmnd"  # Directory to save cached files
 START_DATE = "2023-01-01"
-#END_DATE = "2023-01-07"
 END_DATE = "2025-06-23"
 CHUNK_DAYS = 7  # Number of days per API request
-SLEEP_SECONDS = 5  # Pause between requests to avoid throttling
+PARAMS = {
+    'TAC_AREA_NAME' : 'SCE-TAC',
+}
 
-# Ensure save directory exists
-os.makedirs(SAVE_DIR, exist_ok=True)
-
-def fetch_and_cache_demand(start_date, end_date, chunk_days=7):
-    node = SystemDemand()  
-    
-    start_dt = datetime.fromisoformat(start_date)
-    end_dt = datetime.fromisoformat(end_date)
-    
-    all_chunks = []
-
-    dt = start_dt
-    while dt < end_dt:
-        chunk_end_dt = min(dt + timedelta(days=chunk_days), end_dt)
-        chunk_str = f"{dt.date()}_to_{chunk_end_dt.date()}"
-        cache_file = os.path.join(SAVE_DIR, f"2DA_Demand_{chunk_str}.csv")
-
-        if os.path.exists(cache_file):
-            print(f"[✓] Skipping cached chunk: {chunk_str}")
-            df = pd.read_csv(cache_file, parse_dates=["INTERVALENDTIME_GMT"])
-        else:
-            print(f"[→] Fetching chunk: {chunk_str}")
-            df = node.get_demand_forecast(dt, chunk_end_dt)
-            df.to_csv(cache_file, index=False)
-            time.sleep(SLEEP_SECONDS)
-            #try:
-            #    df = node.get_lmps(dt, chunk_end_dt, market=market)
-            #    df.to_csv(cache_file, index=False)
-            #    time.sleep(SLEEP_SECONDS)
-            #except Exception as e:
-            #    print(f"[!] Failed to fetch {chunk_str}: {e}")
-            #    df = pd.DataFrame()
-
-        all_chunks.append(df)
-        dt = chunk_end_dt
-    
-    df = pd.concat(all_chunks, ignore_index=True)
-    df.to_csv(os.path.join(SAVE_DIR, "dataset.csv"))
-
-    return df
-
-# MAIN EXECUTION
+# Execute data pull
 if __name__ == "__main__":
-    df_full = fetch_and_cache_demand(START_DATE, END_DATE, CHUNK_DAYS)
+    df_full = fetch_and_cache_data(
+        QUERYNAME, 
+        MARKET, 
+        START_DATE, 
+        END_DATE, 
+        SAVE_DIR,
+        PARAMS,
+        chunk_days = CHUNK_DAYS,
+    )
     print(f"\n✅ Downloaded {len(df_full)} rows total.")
     print(df_full.head())
+
