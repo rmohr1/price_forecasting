279229f9db475d547cb8b1db6ba7d9ad432717a3
Rob Mohr <rcmohr4@gmail.com>
2025-07-03 18:26:49 -0400

    added raw and processed data paths

--- src/price_forecasting/config.py
+++ src/price_forecasting/config.py
@@ -3,5 +3,7 @@ from pathlib import Path
 # Base project directory. Dynamic environment resolving can be added later
 PROJECT_DIR = Path(__file__).resolve().parents[2]
 
-DATA_DIR = PROJECT_DIR / "data" #raw and processed data storage location
-MODELS_DIR = PROJECT_DIR / "models" #storage for models and training
+DATA_DIR = PROJECT_DIR / "data" #data storage
+RAW_DATA_DIR = DATA_DIR / "raw" #raw data storage
+PROCESSED_DATA_DIR = DATA_DIR / "processed" #processed data storage
+MODELS_DIR = PROJECT_DIR / "models" #trained models and parameters
