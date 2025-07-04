e76e19e464f22d20bf43264bcd7e6f742a7d03e8
Rob Mohr <rcmohr4@gmail.com>
2025-07-04 11:37:08 -0400

    standardized formatting in scoring_tools

--- src/price_forecasting/utils/scoring_tools.py
+++ src/price_forecasting/utils/scoring_tools.py
@@ -1,24 +1,28 @@
-from properscoring import crps_ensemble
+from typing import Sequence
+
 import numpy as np
+from properscoring import crps_ensemble
+
 
 def get_mean_crps(
-    quantile_preds, 
-    y_true, 
-    quantile_levels,
-    n_interpolated_samples=1000,
-    random_seed=101
-):
-    """
-    Converts predicted quantiles into mean CRPS via synthetic samples in a piecewise-linear interpolation.
+    quantile_preds: Sequence,
+    y_true: Sequence,
+    quantile_levels: Sequence,
+    n_interpolated_samples: int = 1000,
+    random_seed: int = 101,
+) -> float:
+    """Calculate mean CRPS on quantiles by sampling piecewise-linear interpolation.
 
     Args:
-        quantile_preds (array-like): 2-D array of quantile predictions
-        quantile_levels (array-like): Array of quantile levels corresponding to columns of quantile_preds
-        n_interpolated_samples (int): Number of synthetic samples to generate per forecasted point.
-        random_seed (int): random seed for reproducibility
-        
+        quantile_preds: 2-D array of quantile predictions
+        y_true: Array of target values
+        quantile_levels: Array of quantile levels
+        n_interpolated_samples: Number of synthetic samples to generate per point.
+        random_seed: random seed for reproducibility
+
     Returns:
-        float: Mean CRPS score for predictions. 
+        float: Mean CRPS score for predictions.
+
     """
     np.random.seed(random_seed)
     quantile_levels = np.array(quantile_levels)
