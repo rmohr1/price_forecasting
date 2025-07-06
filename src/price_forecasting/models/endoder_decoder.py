be72dbdf55fdefc7a6fdbc9e1df6a8c766ce161a
Rob Mohr <rcmohr4@gmail.com>
2025-07-06 16:57:08 -0400

    added encoder-decoder model file

---
+++ src/price_forecasting/models/endoder_decoder.py
@@ -0,0 +1,30 @@
+from typing import Sequence
+
+import torch.nn as nn
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(
+        self,
+        input_size: int=288,
+        quantiles: Sequence=[0.05, 0.5, 0.95],
+        hidden_size: int=64,
+        num_layers: int=2,
+        dropout: float=0.2,
+    )->nn.Module:
+    
+        super(EncoderDecoder, self).__init__()
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            dropout=dropout,
+            batch_first=True,
+        )
+        self.quantiles = quantiles
+        self.output_layer = nn.Linear(hidden_size, len(quantiles))
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.output_layer(out)
+        return out.squeeze(-1)
