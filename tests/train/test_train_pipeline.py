e551760465ea4da397adec51d13ff969341e12ae
Rob Mohr <rcmohr4@gmail.com>
2025-07-25 14:44:38 -0400

    updated model test for basline LSTM to check new dictionary formatted output

--- tests/train/test_train_pipeline.py
+++ tests/train/test_train_pipeline.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from price_forecasting.train.train_pipeline import load_and_train
 
 
@@ -17,8 +19,13 @@ def test_load_and_train_LSTM(tmp_path):
     assert tmp_path.exists()
     assert (tmp_path / 'config.yaml').exists
     assert (tmp_path / 'model_wts.pt').exists
-    assert (tmp_path / 'y_pred.npy').exists
+    assert (tmp_path / 'y_pred.npz').exists
+    assert (tmp_path / 'y_scaler.pkl').exists
 
+    with np.load(tmp_path / 'y_pred.npz') as y_pred:
+        assert(y_pred['0.5'].shape == (113, 288))
+
+"""
 def test_load_and_train_EncoderDecoder(tmp_path):
     config = {
         "model": "EncoderDecoder",
@@ -37,3 +44,24 @@ def test_load_and_train_EncoderDecoder(tmp_path):
     assert (tmp_path / 'config.yaml').exists
     assert (tmp_path / 'model_wts.pt').exists
     assert (tmp_path / 'y_pred.npy').exists
+
+def test_load_and_train_StudentTMixture(tmp_path):
+    config = {
+        "model": "StudentTLSTM",
+        "hidden_size": 32,
+        "quantiles": [],
+        "learning_rate": 0.01,
+        "batch_size": 100,
+        "epochs": 1,
+        "dropout": 0.1,
+        "loss_function": "log_likelihood",
+        "data_source": 'v1',
+    }
+
+    load_and_train(tmp_path, config=config)
+
+    assert tmp_path.exists()
+    assert (tmp_path / 'config.yaml').exists
+    assert (tmp_path / 'model_wts.pt').exists
+    assert (tmp_path / 'y_pred.npy').exists
+"""
